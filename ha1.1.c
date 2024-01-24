d4a0fb0878b3ee03282a0d16f93bb5493b416db5
Aritra Chowdhury <156349029+AritraChowdhury2005@users.noreply.github.com>
2024-01-24 20:46:42 +0530

    Add files via upload

---
+++ ha1.1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("My BIO-DATA\n");
+    printf("Name:Aritra Chowdhury\nRegd.no:230567890\nBranch:Computer Science\nJee Rank:78980\nGender:Male\nPhone number:9832309456\nAdress:Patia,Bhubaneshwar");
+    return 0;
+}
